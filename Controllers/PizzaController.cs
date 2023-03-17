b6aa89312699e7bbb00bdbf21694ffa42552927c
IÑAKI AGUSTIN PEREZ AVILES <46872121@ort>
2023-03-17 09:37:34 -0300

    creando controller, models, bd

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Pizzas.API.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pizzas.API.Controllers {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PizzasController : ControllerBase {
+        // [HttpGet]
+        // public IActionResult GetAll() {...}
+
+        // [HttpGet("{id}")]
+        // public IActionResult GetById(int id) {...}
+
+        // [HttpPost]
+        // public IActionResult Create(Pizza pizza) {...}
+
+        // [HttpPut("{id}")]
+        // public IActionResult Update(int id, Pizza pizza) {...}
+
+        // [HttpDelete("{id}")]
+        // public IActionResult DeleteById(int id) {...}
+    }
+}
---
+++ Utils/BD.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using Dapper;
+using Pizzas.API.Models;
+
+namespace Pizzas.API.Models{
+    public static class BD {
+        private static string CONNECTION_STRING = "";
+
+        // public static List<Pizza> GetAll() {}
+        // public static Pizza GetById() {}
+        // public static int Insert(Pizza pizza) {}
+        // public static int UpdateById(Pizza pizza) {}
+        // public static int DeleteById(int id) {}
+    }
+}
